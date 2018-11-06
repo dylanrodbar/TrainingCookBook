8590425a307fa0fd68747d76eec6b8651ce1b8f8
dylanrodbar <dylanrodbar97@gmail.com>
2018-11-06 10:31:17 -0600

    Update SecurityConstants.java

--- recetas/src/main/java/initial/constants/SecurityConstants.java
+++ recetas/src/main/java/initial/constants/SecurityConstants.java
@@ -2,7 +2,7 @@ package initial.constants;
 
 public class SecurityConstants {
 	
-	public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final String SECRET = "SecretKeyToGenJWTs";
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
     public static final String TOKEN_PREFIX = "Auth ";
     public static final String HEADER_STRING = "Authorization";
